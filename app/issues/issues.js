63ccd6a7a9e692b6140db1fdfa533bb1144eb826
Jack Polgar <jack@polgar.id.au>
2015-07-01 22:56:45 +1000

    Add show issue page.

--- app/issues/issues.js
+++ app/issues/issues.js
@@ -5,8 +5,16 @@ angular.module('locust.issues', ['ngRoute'])
     $routeProvider.when('/issues', {
       templateUrl: 'issues/index.html',
       controller: 'IssuesCtrl'
+    });
+
+    $routeProvider.when('/issues/:issueId', {
+      templateUrl: 'issues/show.html',
+      controller: 'IssuesDetailCtrl'
     })
   }])
   .controller('IssuesCtrl', ['$scope', 'Issue', function($scope, Issue) {
     $scope.issues = Issue.query();
+  }])
+  .controller('IssuesDetailCtrl', ['$scope', '$routeParams', 'Issue', function($scope, $routeParams, Issue) {
+    $scope.issue = Issue.get({ issueId: $routeParams.issueId });
   }]);
