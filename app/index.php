b1939722989153305900336b321f233e5ee8ed2d
Jack Polgar <jack@polgar.id.au>
2015-07-05 13:50:33 +1000

    Move header into a view to be inlucded into index.

--- app/index.php
+++ app/index.php
@@ -9,39 +9,7 @@
 		<base href="<?= Nanite::baseUri() ?>">
 	</head>
 	<body ng-app="locust">
-		<nav class="navbar navbar-default navbar-static-top">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" ui-sref="roadmap">Locust</a>
-				</div>
-
-				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li ui-sref-active="active"><a ui-sref="roadmap">Roadmap</a></li>
-						<li ui-sref-active="active"><a ui-sref="issues">Issues</a></li>
-						<li ui-sref-active="active"><a ui-sref="new-issue">New Issue</a></li>
-					</ul>
-
-					<ul class="nav navbar-nav navbar-right" ng-if="!currentUser">
-						<li ui-sref-active="active"><a ui-sref="login">Login</a></li>
-						<li ui-sref-active="active"><a ui-sref="register">Register</a></li>
-					</ul>
-
-					<ul class="nav navbar-nav navbar-right" ng-if="currentUser">
-						<li>
-							<a href="#"><i class="glyphicon glyphicon-user"></i></a>
-						</li>
-						<li><a ui-sref="logout">Logout</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<?php require __DIR__ . '/views/header.html'; ?>
 
 		<main>
 			<div ui-view></div>
